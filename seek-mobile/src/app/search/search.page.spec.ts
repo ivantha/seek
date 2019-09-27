87ad76eaa227c47e3b4fccf11a6d08168117bea8
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-09-27 23:10:13 +0200

    Add base mobile structure

--- seek-mobile/src/app/search/search.page.spec.ts
+++ seek-mobile/src/app/search/search.page.spec.ts
@@ -1,21 +1,22 @@
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {TabsPage} from './tabs.page';
+import {SearchPage} from './search.page';
 
-describe('TabsPage', () => {
-    let component: TabsPage;
-    let fixture: ComponentFixture<TabsPage>;
+describe('SearchPage', () => {
+    let component: SearchPage;
+    let fixture: ComponentFixture<SearchPage>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [TabsPage],
+            declarations: [SearchPage],
             schemas: [CUSTOM_ELEMENTS_SCHEMA],
-        }).compileComponents();
+        })
+            .compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(TabsPage);
+        fixture = TestBed.createComponent(SearchPage);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
