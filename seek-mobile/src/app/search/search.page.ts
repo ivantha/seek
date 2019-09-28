8bba2062c21a5b47e5fefbcb72790d906603f36d
Pramodya <pramodyaabeysinghe@gmail.com>
2019-09-28 22:12:48 +0200

    search using distance

--- seek-mobile/src/app/search/search.page.ts
+++ seek-mobile/src/app/search/search.page.ts
@@ -10,6 +10,11 @@ import {FirebaseService} from '../services/firebase.service';
 })
 export class SearchPage implements OnInit {
 
+    _eQuatorialEarthRadius: number = 6378.1370;
+    _d2r: number = (Math.PI / 180.0);
+    myLat: number = 40.111;
+    myLong: number = 2.111;
+
     users: {}[];
 
     searchBarValue: string = '';
@@ -43,19 +48,27 @@ export class SearchPage implements OnInit {
 
     loadUsers() {
         this.fireStore.getUsers().subscribe(data => {
-            if (data) this.users = data.map(e => e.payload.doc.data());
-            else console.log('error getting user details');
+            if (data) {
+                this.users = data.map(e => e.payload.doc.data());
+            } else {
+                console.log('error getting user details');
+            }
         });
     }
 
     getItems() {
         this.initializeItems();
 
-        const keyword = 'physics'; //this.searchBarValue;
+        const keyword = this.searchBarValue;
 
         if (keyword && keyword.trim() != '') {
             this.users.forEach(user => {
                 const skills = user['skills'];
+                const lastKnownLocation = user['lastKnownLocation'];
+                const lat = lastKnownLocation['_lat'];
+                const long = lastKnownLocation['_long'];
+                const dist = this.haversineInKM(this.myLat, this.myLong, lat, long); console.log(dist);
+
                 skills.forEach(skill => {
                     if (this.gender != 'any') {
                         if (skill['name'] === keyword
@@ -63,13 +76,14 @@ export class SearchPage implements OnInit {
                             && user['age'] <= this.maxAge
                             && user['gender'] === this.gender
                             && skill['level'] >= this.skillLevel
+                            && dist <= this.maxDistance
                         ) {
                             this.items.push({
                                 name: user['name'],
                                 age: user['age'],
                                 gender: user['gender'],
                                 skill: skill['level'],
-                                distance: 5
+                                distance: dist
                             });
                         }
                     } else {
@@ -77,13 +91,14 @@ export class SearchPage implements OnInit {
                             && user['age'] >= this.minAge
                             && user['age'] <= this.maxAge
                             && skill['level'] >= this.skillLevel
+                            && dist <= this.maxDistance
                         ) {
                             this.items.push({
                                 name: user['name'],
                                 age: user['age'],
                                 gender: user['gender'],
                                 skill: skill['level'],
-                                distance: 5
+                                distance: dist
                             });
                         }
                     }
@@ -98,13 +113,6 @@ export class SearchPage implements OnInit {
     clearItems() {
         this.items = [];
     }
-
-
-    arrayOne(n: number): any[] {
-        return Array(n);
-    }
-
-
 
     async openModal() {
         const modal = await this.modalController.create({
@@ -127,5 +135,19 @@ export class SearchPage implements OnInit {
         });
 
         return await modal.present();
+    }
+
+    haversineInM(lat1, long1, lat2, long2) {
+        return (1000.0 * this.haversineInKM(lat1, long1, lat2, long2));
+    }
+
+    haversineInKM(lat1, long1, lat2, long2) {
+        let dlong = (long2 - long1) * this._d2r;
+        let dlat = (lat2 - lat1) * this._d2r;
+        let a = Math.pow(Math.sin(dlat / 2.0), 2.0) + Math.cos(lat1 * this._d2r) * Math.cos(lat2 * this._d2r) * Math.pow(Math.sin(dlong / 2.0), 2.0);
+        let c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0 - a));
+        let d = this._eQuatorialEarthRadius * c;
+
+        return d;
     }
 }
