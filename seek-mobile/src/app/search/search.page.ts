c84df2cd24b85fe7ecd75a12c4108f0ec65c4539
Pramodya <pramodyaabeysinghe@gmail.com>
2019-09-28 04:01:43 +0200

    implement basic search

--- seek-mobile/src/app/search/search.page.ts
+++ seek-mobile/src/app/search/search.page.ts
@@ -10,7 +10,32 @@ export class SearchPage implements OnInit {
     constructor() {
     }
 
-    ngOnInit() {
-    }
+    isItemAvailable: boolean = false;
+    items: string[] = [];
 
+	initializeItems(){ 
+	    this.items = ["Physics","Chemistry", "Economics"]; 
+	}
+
+	getItems(ev: any) {
+	    // Reset items back to all of the items
+	    this.initializeItems();
+
+	    // set val to the value of the searchbar
+	    const val = ev.target.value;
+
+	    // if the value is an empty string don't filter the items
+	    if (val && val.trim() != '') {
+	      this.isItemAvailable = true;
+	      this.items = this.items.filter((item) => {
+	        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+	      })
+	    } else {
+	    	this.items = [];
+	    }
+	}
+
+	clearItems(ev: any) {
+		this.items = [];
+	}
 }
